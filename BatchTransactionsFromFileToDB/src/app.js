6cde3728cbfcf2f03ef408c0845820bd23eae3b5
carrin <ct@hellowallet.com>
2015-07-20 22:23:19 -0400

    basic functionality with select for update

--- BatchTransactionsFromFileToDB/src/app.js
+++ BatchTransactionsFromFileToDB/src/app.js
@@ -3,42 +3,71 @@ var Lazy = require("lazy"),
     mysql = require('mysql'),
     config = require('./config.json');
 
-var connection = mysql.createConnection({
+
+
+var lineCounter = 0;
+var updateCounter = 0;
+
+var pool  = mysql.createPool({
     host     : config.db.host,
     user     : config.db.user,
     password : config.db.password,
-    database : 'samples'
+    database : 'samples',
+    waitForConnections: true
 });
-
-connection.connect();
 
 new Lazy(fs.createReadStream('./transactions.txt'))
     .lines.forEach(function(line){
+
+        lineCounter++;
         var tokens = (''+line).split(',');
         var fromAccount = {id: tokens[0], balance: 0};
         var toAccount = {id: tokens[1], balance: 0};
         var transferAmount = parseFloat(tokens[2]);
 
-        connection.query('SELECT * FROM `account` WHERE `id` = ?', [fromAccount.id], function (error, results, fields) {
-            fromAccount.balance = results[0].balance;
-        });
+        pool.getConnection(function(err, connection) {
 
-        connection.query('SELECT * FROM `account` WHERE `id` = ?', [toAccount.id], function (error, results, fields) {
-            toAccount.balance = results[0].balance;
+            connection.beginTransaction(function (err) {
+                if (err) {
+                    throw err;
+                }
 
-            if (fromAccount.balance > transferAmount) {
-                connection.query('UPDATE `account` SET `balance` = ? WHERE `id` = ?', [fromAccount.balance - transferAmount, fromAccount.id], function (error, results, fields) {
+                connection.query('SELECT * FROM `account` WHERE `id` = ? FOR UPDATE', [fromAccount.id], function (error, results, fields) {
+                    fromAccount.balance = results[0].balance;
+
+                    connection.query('SELECT * FROM `account` WHERE `id` = ? FOR UPDATE', [toAccount.id], function (error, results, fields) {
+                        toAccount.balance = results[0].balance;
+                        if (fromAccount.balance < transferAmount) {
+                            transferAmount = 0;
+                        }
+
+
+                        connection.query('UPDATE `account` SET `balance` = ? WHERE `id` = ?', [fromAccount.balance - transferAmount, fromAccount.id], function (error, results, fields) {
+
+                            console.log('about to update. fromAccount: '+JSON.stringify(fromAccount)+', toAccount: '+JSON.stringify(toAccount)+' transferAmount: '+transferAmount);
+                            connection.query('UPDATE `account` SET `balance` = ? WHERE `id` = ?', [toAccount.balance + transferAmount, toAccount.id], function (error, results, fields) {
+                                updateCounter++;
+                                connection.commit(function(err) {
+                                    if (err) {
+                                        return connection.rollback(function() {
+                                            throw err;
+                                        });
+                                    }
+                                    connection.release();
+                                    console.log('success!');
+                                });
+
+
+                                if (lineCounter > 0 && updateCounter == lineCounter) { pool.end(); }
+                            });
+                        });
+                    });
                 });
-
-                connection.query('UPDATE `account` SET `balance` = ? WHERE `id` = ?', [toAccount.balance + transferAmount, toAccount.id], function (error, results, fields) {
-                });
-            }
+            });
         });
-
-
-
-
-
+    }).
+    on('pipe', function() {
+        console.log('lineCounter = '+lineCounter);
     });
 
 
--- BatchTransactionsFromFileToDB/src/createTransactionFile.js
+++ BatchTransactionsFromFileToDB/src/createTransactionFile.js
@@ -9,7 +9,9 @@ for (var ctr = 0;ctr < MAX_TRANSACTIONS; ctr++)
 {
     var account1 = Math.floor(Math.random()*config.data.maxAccounts);
     var account2 = Math.floor(Math.random()*config.data.maxAccounts);
-    var amount = Math.floor(Math.random()*config.data.initialBalance*100)/100;
+    var amount = Math.floor(Math.random()*config.data.initialBalance);
+
+    if (account1 == account2) { continue; }
 
     var line = ''+account1+','+account2+','+amount+'\n';
     fs.appendFileSync('./transactions.txt', line);
